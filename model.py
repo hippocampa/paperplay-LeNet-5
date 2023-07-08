ce79c3f54d6d30a90a6f6918579aceae801d541c
hippocampa <tsdhrm@outlook.com>
2023-07-08 11:39:18 +0800

    todo: create model

---
+++ model.py
@@ -0,0 +1,10 @@
+import torch
+from torch import nn
+
+
+class LeNet5(nn.Module):
+    def __init__(self) -> None:
+        super(LeNet5).__init__()
+
+    def forward(self, x):
+        pass
--- train.py
+++ train.py
@@ -1,9 +1,25 @@
 import fire
+from model import LeNet5
+from utils import RANDOM_SEED
+from loader import load_dataset
+from prepdata import make_dataset
+import torch
 
 
-def train(model, batch_size, learning_rate, epochs):
-    print(model, batch_size, learning_rate, epochs)
+def main(batch_size: int = 32, learning_rate: float = 0.01, epochs: int = 2):
+    """
+    How to run the code:
+        input: python3 train.py --batch_size b --learning_rate lr --epochs e
+    ---
+    Train the dataset
+    return None
+    """
+    torch.manual_seed(RANDOM_SEED)
+    train_data, test_data = make_dataset()
+    train_loader = load_dataset(data=train_data, batchsize=batch_size, is_shuffle=True)
+    test_loader = load_dataset(data=test_data, batchsize=batch_size, is_shuffle=False)
+    model = LeNet5()
 
 
 if __name__ == "__main__":
-    fire.Fire(train)
+    fire.Fire(main)
